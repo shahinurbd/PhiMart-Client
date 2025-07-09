07d83a7790145c934587be799d4460a4a240c39a
Muhammad Shahinur <shahinurislam728@gmail.com>
2025-07-09 17:36:02 +0600

    issu fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import HeroCarousel from '../components/Carousel/HeroCarousel';
-import Features from '../components/features';
+import Features from '../components/Features';
 import Product from '../components/Products/Product';
 import Discount from '../components/Discount/DiscountSection';
 import Category from '../components/Categories/Category';
